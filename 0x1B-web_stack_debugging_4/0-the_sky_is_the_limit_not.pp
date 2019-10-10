c872d748b307f9f26b0b9b15d0b79f271df609f1
Mark Hedgeland <mhedgeland@gmail.com>
2019-10-10 13:52:31 -0400

    [GLOBAL][pp] Slight changes

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,12 @@
+#Fixes low file limit in nginx server
 file { 'replace last line':
   ensure   => present,
   path     => '/etc/default/nginx',
   content  => 'ULIMIT="-n 4096"',
 }
 
-service { 'nginx':
-  ensure    => running,
-  subscribe => File['/etc/default/nginx']
+service { 'Nginx':
+  ensure      => running,
+  hasrestart  => true,
+  subscribe   => File['/etc/default/nginx']
 }
